f0d85dfe80a3cf0ffd0cc10a54a50e3aca3af80e
Leandro0101 <leandrovieira3306@gmail.com>
2021-01-31 15:56:52 -0300

    test: ensure addParticipantToGiveAway return undefined if shares number participants to be less 20

--- src/data/services/AddParticipantToGiveAway.ts
+++ src/data/services/AddParticipantToGiveAway.ts
@@ -1,12 +1,12 @@
 import { IGiveAway } from '../../domain/entities/giveaway'
-import { Participant } from '../../domain/entities/participant'
+import { IParticipant } from '../../domain/entities/participant'
 import { IAddParticipantToGiveAway } from '../../domain/usecases/addParticipantToGiveAway'
 
 export class AddParticipantToGiveAwayService implements IAddParticipantToGiveAway {
   constructor (private readonly giveaway: IGiveAway) { }
-  async execute (participant: Participant): Promise<Participant | undefined> {
+  async execute (participant: IParticipant): Promise<IParticipant | undefined> {
     if (!this.giveaway.verifyPartipantShares(participant)) {
-      throw new Error('Shares number must be greater or equal than 20')
+      return undefined
     }
 
     await this.giveaway.addParticipant(participant)
---
+++ src/data/services/addParticipantToGiveAway.spec.ts
@@ -0,0 +1,21 @@
+import { GiveAwayModel } from '../models/GiveAway'
+import { ParticipantModel } from '../models/Participant'
+import { AddParticipantToGiveAwayService } from './AddParticipantToGiveAway'
+
+describe('add participant to giveaway', () => {
+  const giveaway = new GiveAwayModel('HPT002',
+    {
+      id: 'XUA002',
+      name: 'TV de plasma',
+      description: 'TV seminova',
+      imgUrl: 'https://www.android.com/static/2016/img/devices/tvs/transparent/sharp-aquos_1x.png'
+    },
+    new Date())
+
+  const sut = new AddParticipantToGiveAwayService(giveaway)
+  test('Should return undefined if participant shares number be less than 20', async () => {
+    const participant = new ParticipantModel('098f6bcd4621d373cade4e832627b4f6', 19, 'Leandro', 'leandro@gmail.com')
+    const response = await sut.execute(participant)
+    expect(response).toBe(undefined)
+  })
+})
