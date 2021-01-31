547b9df38786fac276345028d1d3b946ac1c00d8
Leandro0101 <leandrovieira3306@gmail.com>
2021-01-31 15:27:50 -0300

    feat: add ParticipantModel at data layer

---
+++ src/data/models/Participant.ts
@@ -0,0 +1,5 @@
+import { IParticipant } from '../../domain/entities/participant'
+
+export class ParticipantModel implements IParticipant {
+  constructor (public readonly id: string, public shares: Number, public name: string, public email: string) { }
+}
