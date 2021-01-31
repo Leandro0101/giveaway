f99c8083246ef85c8be7b01bcffdcbe085b88952
Leandro0101 <leandrovieira3306@gmail.com>
2021-01-31 11:57:12 -0300

    feat: type Participant at domain layer

---
+++ src/domain/entities/participant.ts
@@ -0,0 +1,5 @@
+export type Participant = {
+  shares: Number
+  name: string
+  email: string
+}
---
+++ src/test.spec.ts
@@ -0,0 +1,5 @@
+describe('test', () => {
+  test('should pass', () => {
+    expect(1).toBe(1)
+  })
+})
