b07649fa2e2e5a794b9ea946b9fede5cf4436f4a
Leandro0101 <leandrovieira3306@gmail.com>
2021-01-31 14:46:11 -0300

    refactor: change method's return verifyParticipantShares to boolean at giveaway domain

--- src/domain/entities/giveaway.ts
+++ src/domain/entities/giveaway.ts
@@ -10,5 +10,5 @@ export interface IGiveAway {
   addParticipant: (participant: Participant) => Promise<void>
   removeParticipant: (participant: Participant) => Promise<void>
   draw: () => Promise<Participant>
-  verifyPartipantShares: (participants: Participant) => Promise<void>
+  verifyPartipantShares: (participants: Participant) => boolean
 }
