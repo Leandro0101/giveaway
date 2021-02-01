01a3d8d4913d7de3bf65ae70b61b7633fb49beda
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-01 19:11:16 -0300

    feat: add dataSource's participants at infra layer

---
+++ src/infra/dataSources/participant.ts
@@ -0,0 +1,34 @@
+import { IParticipant } from '../../domain/entities/participant'
+
+export const participantDataSource: IParticipant[] = [
+  {
+    id: '098f6bcd4621d373cade4e832627b4f6',
+    shares: 27,
+    name: 'Leandro Lima',
+    email: 'leandro@gmail.com'
+  },
+  {
+    id: '192f6bcd4621d373cade4e832627b4k9',
+    shares: 25,
+    name: 'José Wilker',
+    email: 'jose@gmail.com'
+  },
+  {
+    id: '539f6bcd4621d373cade4e832627b4f1',
+    shares: 22,
+    name: 'Vitor Lucas',
+    email: 'vitor@gmail.com'
+  },
+  {
+    id: '000f6bcd4621d373cade4e832627b4f2',
+    shares: 20,
+    name: 'Andrew Monteiro',
+    email: 'andrew@gmail.com'
+  },
+  {
+    id: '243f6bcd4621d373cade4e832627b4f0',
+    shares: 26,
+    name: 'João Victor',
+    email: 'joao@gmail.com'
+  }
+]
