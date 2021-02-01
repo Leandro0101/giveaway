023bb67f9e9ff49fdfd281708da37301a5bd09a5
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-01 20:44:24 -0300

    feat: add dataSource's items at infra layer

---
+++ src/infra/dataSources/giveaway.ts
@@ -0,0 +1,25 @@
+// import { IGiveAway } from '@domain/entities/giveaway'
+// import { GiveAwayModel } from 'src/data/models/GiveAway'
+
+// export const giveawayDataSource: IGiveAway[] = [
+//   {
+//     id: 'HPT002',
+//     {
+
+//     }
+//   }
+// ]
+
+// const giveaway1 = new GiveAwayModel('HPT002',
+// {
+//   id: 'XUA002',
+//   name: 'TV de plasma',
+//   description: 'TV seminova',
+//   imgUrl: 'https://www.android.com/static/2016/img/devices/tvs/transparent/sharp-aquos_1x.png'
+// },
+// new Date())
+
+// id: string
+// item: IItem
+// participants: IParticipant[]
+// date: Date
---
+++ src/infra/dataSources/item.ts
@@ -0,0 +1,54 @@
+import { IItem } from '@domain/entities/item'
+
+const item1: IItem = {
+  id: 'XUA002',
+  name: 'TV de plasma',
+  description: 'TV seminova',
+  imgUrl: 'https://www.android.com/static/2016/img/devices/tvs/transparent/sharp-aquos_1x.png'
+}
+
+const item2: IItem = {
+  id: 'XUA003',
+  name: 'PlayStation 4',
+  description: 'PlayStation seminovo',
+  imgUrl: 'https://www.android.com/static/2016/img/devices/tvs/transparent/sharp-aquos_1x.png'
+}
+
+const item3: IItem = {
+  id: 'XUA004',
+  name: 'Bike',
+  description: 'Bike seminova',
+  imgUrl: 'https://www.android.com/static/2016/img/devices/tvs/transparent/sharp-aquos_1x.png'
+}
+
+const item4: IItem = {
+  id: 'XUA005',
+  name: 'Monitor',
+  description: 'Monitor seminovo',
+  imgUrl: 'https://www.android.com/static/2016/img/devices/tvs/transparent/sharp-aquos_1x.png'
+}
+
+const item5: IItem = {
+  id: 'XUA006',
+  name: 'Cadeira gamer',
+  description: 'Cadeira gamer seminova',
+  imgUrl: 'https://www.android.com/static/2016/img/devices/tvs/transparent/sharp-aquos_1x.png'
+}
+
+const item6: IItem = {
+  id: 'XUA007',
+  name: 'Iphone',
+  description: 'Iphone seminovo',
+  imgUrl: 'https://www.android.com/static/2016/img/devices/tvs/transparent/sharp-aquos_1x.png'
+}
+
+const item7: IItem = {
+  id: 'XUA008',
+  name: 'Computador',
+  description: 'Computador seminovo',
+  imgUrl: 'https://www.android.com/static/2016/img/devices/tvs/transparent/sharp-aquos_1x.png'
+}
+
+export const itemDataSource: IItem[] = [
+  item1, item2, item3, item4, item5, item6, item7
+]
