a60fcca0a621e8adb8b7c79a6923d6ed94766653
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-02 18:26:14 -0300

    test: ensure the return of the created participant from addParticipantToGiveAwayService

--- src/data/services/addParticipantToGiveAway/addParticipantToGiveAway.spec.ts
+++ src/data/services/addParticipantToGiveAway/addParticipantToGiveAway.spec.ts
@@ -1,22 +1,25 @@
-// import { giveawayDataSource } from '@/src/infra/dataSources/giveaway'
-// import { GiveAwayModel } from '../../models/GiveAway'
-// import { ParticipantModel } from '../../models/Participant'
-// import { AddParticipantToGiveAwayService } from './AddParticipantToGiveAway'
+import { IAddParticipantToGiveAway } from '@/src/domain/usecases/addParticipantToGiveAway'
+import { giveawayDataSource } from '@/src/infra/dataSources/giveaway'
+import { ParticipantModel } from '../../models/Participant'
+import { AddParticipantToGiveAwayService } from './AddParticipantToGiveAway'
+import { FindGiveAwayByIdRepository } from '../../../infra/repositories/FakeFindGiveAwayById'
+import { IFindGiveAwayByIdRepository } from '../../contracts/repositories/findGiveAwayById'
+import { AddParticipantToGiveAwayRepository } from '@/src/infra/repositories/FakeAddParticipantToGiveAway'
+import { IAddParticipantToGiveAwayRepository } from '../../contracts/repositories/addParticipantToGiveAway'
 
 describe('add participant to giveaway', () => {
-  // const giveaway = new GiveAwayModel('HPT002',
-  //   {
-  //     id: 'XUA002',
-  //     name: 'TV de plasma',
-  //     description: 'TV seminova',
-  //     imgUrl: 'https://www.android.com/static/2016/img/devices/tvs/transparent/sharp-aquos_1x.png'
-  //   },
-  //   new Date())
+  const makeSut = (): IAddParticipantToGiveAway => {
+    const findGiveAwayById: IFindGiveAwayByIdRepository = new FindGiveAwayByIdRepository(giveawayDataSource)
+    const addParticipantToGiveAway: IAddParticipantToGiveAwayRepository = new AddParticipantToGiveAwayRepository()
 
-  // const sut = new AddParticipantToGiveAwayService(giveaway)
-  test('Should return undefined if participant shares number be less than 20', async () => {
-    // const participant = new ParticipantModel('098f6bcd4621d373cade4e832627b4f6', 19, 'Leandro', 'leandro@gmail.com')
-    // const response = await sut.execute(giveawayDataSource[0], participant)
-    expect(1).toBe(1)
+    return new AddParticipantToGiveAwayService(findGiveAwayById, addParticipantToGiveAway)
+  }
+
+  const sut: IAddParticipantToGiveAway = makeSut()
+
+  test('Should return the added participant', async () => {
+    const participant = new ParticipantModel('098f6bcd4621d373cade4e832627b4f6', 19, 'Leandro', 'leandro@gmail.com')
+    const response = await sut.execute('XPA01', participant)
+    expect(response).toBe(participant)
   })
 })
