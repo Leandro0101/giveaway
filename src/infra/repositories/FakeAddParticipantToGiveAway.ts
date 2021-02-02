7b4325db4e7aaff4bfdae5c14f7ad93ab7492711
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-02 18:36:33 -0300

    refactor: remove file duplicate

--- src/infra/repositories/FakeAddParticipantToGiveAway.ts
+++ src/infra/repositories/FakeAddParticipantToGiveAway.ts
@@ -1,6 +1,6 @@
 import { IGiveAway } from '@domain/entities/giveaway'
 import { IAddParticipantToGiveAwayRepository } from '../../data/contracts/repositories/addParticipantToGiveAway'
-import { IParticipant } from '../../domain/entities/participant'
+import { IParticipant } from '@domain/entities/participant'
 import { participantDataSource } from '../dataSources/participant'
 export class AddParticipantToGiveAwayRepository implements IAddParticipantToGiveAwayRepository {
   async addParticipant (giveaway: IGiveAway, participant: IParticipant): Promise<IParticipant> {
