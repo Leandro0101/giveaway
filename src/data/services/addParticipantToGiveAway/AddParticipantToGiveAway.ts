8f5543439d8befb36797104914db0dc16b1e8184
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-02 18:24:01 -0300

    refactor: inject addParticipantToGiveAwayRepository as dependency

--- src/data/services/addParticipantToGiveAway/AddParticipantToGiveAway.ts
+++ src/data/services/addParticipantToGiveAway/AddParticipantToGiveAway.ts
@@ -1,20 +1,22 @@
-import { IGiveAway } from '@domain/entities/giveaway'
 import { IParticipant } from '@domain/entities/participant'
 import { IAddParticipantToGiveAway } from '@domain/usecases/addParticipantToGiveAway'
+import { IAddParticipantToGiveAwayRepository } from '../../contracts/repositories/addParticipantToGiveAway'
 import { IFindGiveAwayByIdRepository } from '../../contracts/repositories/findGiveAwayById'
 
 export class AddParticipantToGiveAwayService implements IAddParticipantToGiveAway {
-  constructor (private readonly findGiveAwayById: IFindGiveAwayByIdRepository, private readonly giveaway: IGiveAway) { }
-  async execute (giveawayId: string, participant: IParticipant): Promise<IParticipant | undefined> {
-    if (!this.giveaway.verifyPartipantShares(participant)) {
-      return undefined
-    }
+  constructor (
+    private readonly findGiveAwayById: IFindGiveAwayByIdRepository,
+    private readonly addParticipantToGiveAwayRepository: IAddParticipantToGiveAwayRepository
+  ) { }
 
+  async execute (giveawayId: string, participant: IParticipant): Promise<IParticipant | undefined> {
     const giveaway = await this.findGiveAwayById.findById(giveawayId)
 
     if (!giveaway) {
       throw new Error('giveaway not found')
     }
+
+    await this.addParticipantToGiveAwayRepository.addParticipant(giveaway, participant)
 
     return participant
   }
