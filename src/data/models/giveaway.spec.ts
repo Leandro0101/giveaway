278d162e5b30f7e0e7b28af8e905b2c31f7abf24
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-03 17:59:04 -0300

    test: ensure that addParticipant at GiveAwayModel add a participant

---
+++ src/data/models/giveaway.spec.ts
@@ -0,0 +1,15 @@
+import { IGiveAway } from '@/src/domain/entities/giveaway'
+import { IParticipant } from '@/src/domain/entities/participant'
+import { itemDataSource } from '@/src/infra/dataSources/item'
+import { GiveAwayModel } from './GiveAway'
+import { ParticipantModel } from './Participant'
+
+describe('test giveaway methods', () => {
+  const sut: IGiveAway = new GiveAwayModel('XPA03', itemDataSource[0], new Date())
+  const participant: IParticipant = new ParticipantModel('098f6bcd4621d373cade4e832627b4f6', 21, 'Leandro', 'leandro@gmail.com')
+
+  test('Should add participant to giveaway', async () => {
+    await sut.addParticipant(participant)
+    expect(sut.participants.length).toBe(1)
+  })
+})
